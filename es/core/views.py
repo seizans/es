d0a95be9bc91179a459aa164f646688745d9417e
Seizan Shimazaki <seizans@gmail.com>
2014-01-08 14:13:10 +0900

    データを作って表示する

--- es/core/views.py
+++ es/core/views.py
@@ -1,7 +1,19 @@
 # coding=utf8
 
-from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from core.models import Note
 
 
-def home(request):
-    return render(request, 'home.html', {'from_hello_view': 'hoge'})
+class HomeView(CreateView):
+    model = Note
+    template_name = 'home.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['notes'] = Note.objects.all()
+        return context
+
+home = HomeView.as_view()
